fc9c33694e10131e202de99112d14250ad5609df
spsfhandson <104430860+spsfhandson@users.noreply.github.com>
2022-04-26 12:05:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,12 @@
 import streamlit
 
 streamlit.title('Happy to Set up my first website app')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Chicken Biryani £10.00')
+
+streamlit.text('Mutton Biryani £15.00')
+
+streamlit.text('FIsh Biryani £20.00')
+
