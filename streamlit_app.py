6f9145f293d3bd0f62195bb386f3aa903c76778e
spsfhandson <104430860+spsfhandson@users.noreply.github.com>
2022-04-26 12:06:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,11 @@ streamlit.title('My Hotel App Website')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Chicken Biryani £10.00')
-
 streamlit.text('Mutton Biryani £15.00')
-
 streamlit.text('FIsh Biryani £20.00')
 
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
