a5e0b24b8ff5c903df0d5e336e4676db82e746d3
spsfhandson <104430860+spsfhandson@users.noreply.github.com>
2022-04-26 12:08:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-
+import pandas
 import streamlit
 
 streamlit.title('My Hotel App Website')
@@ -16,3 +16,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
