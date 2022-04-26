ee652812d657e0868905ffa839e6eac8465d2635
spsfhandson <104430860+spsfhandson@users.noreply.github.com>
2022-04-26 12:07:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,11 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Chicken Biryani £10.00')
 streamlit.text('Mutton Biryani £15.00')
 streamlit.text('FIsh Biryani £20.00')
-
-
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
