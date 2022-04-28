022ab2003f3258600e28ad1670ab7287b9c465f9
spsfhandson <104430860+spsfhandson@users.noreply.github.com>
2022-04-28 09:57:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas
 import streamlit
+import requests
 
 streamlit.title('My Hotel App Website')
 
@@ -27,7 +28,12 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected] 
 
-
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
+
 
 
